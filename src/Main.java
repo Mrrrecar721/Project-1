51aba8ef7514e5a6c853f76eca866299449796c6
Rich R <mrrrecar721@gmail.com>
2023-10-06 16:23:33 -0400

    Task Class almost complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 // Richard Recar
 // CSCI-340 Project-1
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello there, lets get it");
+
+        Task t1 = new Task("T1", 1, 20);
+
+//        ArrayList<Task> taskSet = new ArrayList<>();
+        t1.printTask();
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -2,4 +2,14 @@ public class Task {
     String taskName = " ";
     int priority;
     int burst;
+
+    Task(String name, int priority, int burst){
+        this.taskName = name;
+        this.priority = priority;
+        this.burst = burst;
+    }
+
+    public void printTask(){
+        System.out.println(taskName + "\n" + priority + "\n" + burst);
+    }
 }
